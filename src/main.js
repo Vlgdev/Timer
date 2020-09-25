61c8e5af2e04b0cd783ba6ed28c9cdc527a26483
Vlgdev <vig-dev@mail.ru>
2020-09-25 09:28:36 +0300

    Set production tip and add public path

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import timeFilter from './filters/time.filter'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 Vue.filter('time', timeFilter)
 
 new Vue({
